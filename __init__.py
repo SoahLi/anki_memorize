884a5ffe014f3912ab0b0886408ad58bcf660138
SoahLi <ows8turnbull@gmail.com>
2026-05-20 12:09:13 -0700

    create packages for each directory by adding an init.py

--- __init__.py
+++ __init__.py
@@ -6,6 +6,6 @@ import sys
 # Add the "lib" directory to the Python path so anki can find our dependencies.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 
-from .main import main
+from main import main
 
 main()
